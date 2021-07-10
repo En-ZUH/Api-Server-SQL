3743232b71aa768ecad7664d3271f178ea1084c5
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-07-10 23:44:36 +0300

    done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 require('dotenv').config();
-const mongoose = require('mongoose');
+
 const server = require('./src/server');
 const pool = require('./src/models/pool');
+
+
 
 pool.connect().then(() => {
     server.startServer(process.env.PORT || 4000);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 'use strict';
-const cors = require('cors');
+
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const cors = require('cors');
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
 
 
 const PORT = process.env.PORT || 4500;
